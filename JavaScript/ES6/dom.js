a0044bca2aea52a0511313cb002a70702d8534bd
agaryangupta <agaryangupta123@gmail.com>
2025-10-13 11:58:37 +0530

    dom

---
+++ JavaScript/ES6/dom.js
@@ -0,0 +1,9 @@
+const div = document.createElement('div');
+const h1 = document.createElement('h1');
+
+h1.innerHTML = "Hello, World!";
+
+div.appendChild(h1);
+
+const id = document.getElementById('root');
+id.appendChild(div);
