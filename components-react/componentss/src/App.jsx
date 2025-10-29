e7f8515c38089fe8353ce1018fbdd53dc0ee4b02
agaryangupta <agaryangupta123@gmail.com>
2025-10-29 10:00:17 +0530

    nabvar

--- components-react/componentss/src/App.jsx
+++ components-react/componentss/src/App.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import { Card } from './ccomponents/Card'
+import  Navbar  from './ccomponents/Navbar'
+
 import './app.css'
 const App = () => {
   return (
+    <>
+    <Navbar/>
     <div id ="modify">
-      <Card name={"Hi"}/>
-      <Card name={"my"}/>
+      <Card name={"My"}/>
       <Card name={"name"}/>
       <Card name={"is"}/>
       <Card name={"Aryan"}/>
       <Card name={"Gupta"}/>
     </div>
+    </>
   )
 }
 
---
+++ components-react/componentss/src/ccomponents/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './navbar.css'
+
+function Navbar() {
+  return (
+    <div>
+      <div>
+        <ul id='nav'>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+            <li>Cards</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
