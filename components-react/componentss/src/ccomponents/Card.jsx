18c49eae731eb92b521445c1586aeab8a98258ed
agaryangupta <agaryangupta123@gmail.com>
2025-10-29 09:43:15 +0530

    card

--- components-react/componentss/src/ccomponents/Card.jsx
+++ components-react/componentss/src/ccomponents/Card.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import './card.css'
-export const Card = () => {
+export const Card = (props) => {
   return (
     <div id="ary-card">
         <img src ="https://wallpapersok.com/images/high/shiro-and-shin-chan-iphone-ao1ij09557z5nxt2.jpg" alt="" height={100} width={100}></img>
-        <h1>Aryan</h1>
+        <h1>{props.name}</h1>
     </div>
   )
 }
