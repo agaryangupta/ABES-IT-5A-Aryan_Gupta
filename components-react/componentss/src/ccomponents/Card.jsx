ed7a4009b5b1315e9fff01f1f29ba2256b0b1445
agaryangupta <agaryangupta123@gmail.com>
2025-10-29 09:19:41 +0530

    css,component

---
+++ components-react/componentss/src/ccomponents/Card.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './card.css'
+export const Card = () => {
+  return (
+    <div id="ary-card">
+        <img src ="https://wallpapersok.com/images/high/shiro-and-shin-chan-iphone-ao1ij09557z5nxt2.jpg" alt="" height={100} width={100}></img>
+        <h1>Aryan</h1>
+    </div>
+  )
+}
