b6690918e73291aca52a503f9965dce870d25dc4
agaryangupta <agaryangupta123@gmail.com>
2025-11-03 12:50:01 +0530

    done

---
+++ assignments/book-app/book/src/components/book.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './book.css'
+function Book() {
+  return (
+    <div id='book'>
+        <img src="" alt="" />
+        <h1>Title:Math</h1>
+        <h1>290</h1>
+    </div>
+  )
+}
+
+export default Book
