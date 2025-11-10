cc5fc387a39987741df58403156ff02dab4df59f
agaryangupta <agaryangupta123@gmail.com>
2025-11-10 11:57:25 +0530

    done

--- assignments/book-app/book/src/components/Navbar.jsx
+++ assignments/book-app/book/src/components/Navbar.jsx
@@ -6,6 +6,8 @@ function Navbar() {
     <nav className="navbar">
       <div className="nav-left">
         <button className="nav-btn">Home</button>
+        <button className="nav-btn">About</button>
+        <button className="nav-btn">Contact</button>
       </div>
       <div className="nav-right">
         <button className="nav-btn">Cart</button>
