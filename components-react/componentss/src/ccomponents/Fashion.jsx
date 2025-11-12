208ad90ca0ddd899bba3e94291e4ace7d586eb88
agaryangupta <agaryangupta123@gmail.com>
2025-11-12 09:19:22 +0530

    fashion

---
+++ components-react/componentss/src/ccomponents/Fashion.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Fashion = (props) => {
+  return (
+    <div>
+        <div>
+            <h1>Fashion Society ABES</h1>
+        </div>
+        <div>
+            <img src={props.image} alt="" height = {80} width={80} />
+            <h4>Title:{props.title}</h4>
+            <h4>Price:{props.price}</h4>
+        </div>
+    </div>
+  )
+}
